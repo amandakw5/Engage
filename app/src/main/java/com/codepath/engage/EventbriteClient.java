65c6769bd45e633c6eb7d72492485afb330d02f7
Daniel Calderon Manriquez <calderond@fb.com>
2017-07-11 17:00:39 -0700

    Added Ui Bar

--- app/src/main/java/com/codepath/engage/EventbriteClient.java
+++ app/src/main/java/com/codepath/engage/EventbriteClient.java
@@ -1,7 +1,5 @@
 package com.codepath.engage;
 
-import android.util.Log;
-
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 
@@ -13,14 +11,15 @@ public class EventbriteClient {
 
     static final String baseApiUrl = "https://www.eventbriteapi.com/v3/";
     static final String searchBaseUrl="events/search/?q=";
-    static final String endBaseUrl = "&token="+Integer.toString(R.string.EventBriteKey);
+    //TODO fix secret key wiht authentication token
+    static final String endBaseUrl = "&token=ZVEFG6DHKZONNTXLL5HM";
 
     public String finalUrl="";
     private static AsyncHttpClient client = new AsyncHttpClient();
 
 
     public void getInfoByQuery(String query, AsyncHttpResponseHandler asyncHttpResponseHandler){
-        Log.i("key",R.string.EventBriteKey);
+
         finalUrl = baseApiUrl+searchBaseUrl+query
                 +endBaseUrl;
         client.get(finalUrl,asyncHttpResponseHandler);
---
+++ app/src/main/java/com/codepath/engage/models/Event.java
@@ -0,0 +1,9 @@
+package com.codepath.engage.models;
+
+/**
+ * Created by calderond on 7/11/17.
+ */
+
+public class Event {
+    public String;
+}
