f197ac57f77c01babe5434a3af4829e972696726
Emily Zhao <emilyz@fb.com>
2017-07-12 14:20:31 -0700

    allows user to stay in the app even when closed and added layout for even details

---
+++ app/src/main/java/com/codepath/engage/EventDetailsActivity.java
@@ -0,0 +1,15 @@
+package com.codepath.engage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EventDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().setTitle("Event Name");
+        setContentView(R.layout.activity_event_details);
+
+    }
+}
--- app/src/main/java/com/codepath/engage/LoginActivity.java
+++ app/src/main/java/com/codepath/engage/LoginActivity.java
@@ -1,40 +1,71 @@
 package com.codepath.engage;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.loopj.android.http.Base64;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class LoginActivity extends AppCompatActivity {
 
     public static CallbackManager mCallbackManager;
+    private String TAG = "TOKEN_ACCESS";
+    private AccessToken token;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
         mCallbackManager = CallbackManager.Factory.create();
 
-        getSupportActionBar().setTitle("Log In");
+        getSupportActionBar().hide();
 
         setContentView(R.layout.activity_login);
+
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo("com.codepath.engage", PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+
+        } catch (NoSuchAlgorithmException | PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+
+        }
+
+        if (AccessToken.getCurrentAccessToken()!=null){
+            Intent i = new Intent(LoginActivity.this, EventDetailsActivity.class);
+            startActivity(i);
+        }
 
         LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
         loginButton.setReadPermissions("user_friends");
         loginButton.setReadPermissions("public_profile");
-        loginButton.setReadPermissions("email");
-        loginButton.setReadPermissions("user_birthday");
         loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-//                Intent intent = new Intent(LoginActivity.this, HomePage.class);
-//                startActivity(intent);
+
+                Intent intent = new Intent(LoginActivity.this, EventDetailsActivity.class);
+                startActivity(intent);
             }
 
             @Override
@@ -48,7 +79,12 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
         mCallbackManager.onActivityResult(requestCode, resultCode, data);
+        token = AccessToken.getCurrentAccessToken();
+        if (token != null) {
+            Toast.makeText(this, token.toString(), Toast.LENGTH_SHORT).show();
+            Log.e(TAG, "Token: " + token.getToken());
+            Log.e(TAG, "UserID: " + token.getUserId());
+        }
     }
 }
