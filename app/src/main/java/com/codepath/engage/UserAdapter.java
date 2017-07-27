6c47db35e9e2a0f53556a727366ed93a88d3b956
Daniel Calderon Manriquez <calderond@fb.com>
2017-07-27 10:59:44 -0700

    Added adpater for user

--- app/src/main/java/com/codepath/engage/UserAdapter.java
+++ app/src/main/java/com/codepath/engage/UserAdapter.java
@@ -1,8 +1,57 @@
 package com.codepath.engage;
 
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.codepath.engage.models.User;
+
+import java.util.List;
+
 /**
  * Created by calderond on 7/27/17.
  */
 
-public class UserAdapter {
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder>{
+    private List<User> users;
+    private Context context;
+    private int recyclerType;
+    View recyclerView;
+
+    public UserAdapter(List<User> users,int i){
+        this.users = users;
+        recyclerType = i;
+    }
+
+    @Override
+    public UserAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(UserAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+    public class ViewHolder extends RecyclerView.ViewHolder implements  View.OnClickListener{
+        public TextView tvEventName;
+        public ImageView profileImage;
+        public TextView name;
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+        }
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
 }
