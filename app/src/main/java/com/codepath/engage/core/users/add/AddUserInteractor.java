0c115281fd932b994eb6086b5f56a9af84e6726d
Daniel Calderon Manriquez <calderond@fb.com>
2017-07-31 14:19:43 -0700

    Added messaging

---
+++ app/src/main/java/com/codepath/engage/core/users/add/AddUserInteractor.java
@@ -0,0 +1,45 @@
+package com.codepath.engage.core.users.add;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import com.codepath.engage.R;
+import com.codepath.engage.models.UserChat;
+import com.codepath.engage.utils.Constants;
+import com.codepath.engage.utils.SharedPrefUtil;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
+
+public class AddUserInteractor implements AddUserContract.Interactor {
+    private AddUserContract.OnUserDatabaseListener mOnUserDatabaseListener;
+
+    public AddUserInteractor(AddUserContract.OnUserDatabaseListener onUserDatabaseListener) {
+        this.mOnUserDatabaseListener = onUserDatabaseListener;
+    }
+
+    @Override
+    public void addUserToDatabase(final Context context, FirebaseUser firebaseUser) {
+        DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+        UserChat user = new UserChat(firebaseUser.getUid(),
+                firebaseUser.getEmail(),
+                new SharedPrefUtil(context).getString(Constants.ARG_FIREBASE_TOKEN));
+        database.child(Constants.ARG_USERS)
+                .child(firebaseUser.getUid())
+                .setValue(user)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            mOnUserDatabaseListener.onSuccess(context.getString(R.string.user_successfully_added));
+                        } else {
+                            mOnUserDatabaseListener.onFailure(context.getString(R.string.user_unable_to_add));
+                        }
+                    }
+                });
+    }
+}
